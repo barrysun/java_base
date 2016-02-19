5a892340512b983193e03ce42fb4854efc4f135d
barry <1096490965@qq.com>
2016-02-19 17:39:13 +0800

    Block HttpServer

--- netTest/src/com/ch05/SimpleHttpServer.java
+++ netTest/src/com/ch05/SimpleHttpServer.java
@@ -15,107 +15,111 @@ import java.util.concurrent.Executors;
 
 /**
  * 阻塞模式
+ * 
  * @author apple
  *
  */
 public class SimpleHttpServer {
-	
-	private int port=80;
-	private ServerSocketChannel serverSocketChannel=null;
+
+	private int port = 8080;
+	private ServerSocketChannel serverSocketChannel = null;
 	private ExecutorService executorService;
-	private static final int POOL_MULTIPLE=4;
-	
-	public SimpleHttpServer() throws IOException{
-		executorService=Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors() * POOL_MULTIPLE);
-		serverSocketChannel=ServerSocketChannel.open();
+	private static final int POOL_MULTIPLE = 4;
+
+	public SimpleHttpServer() throws IOException {
+		executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors() * POOL_MULTIPLE);
+		serverSocketChannel = ServerSocketChannel.open();
 		serverSocketChannel.socket().setReuseAddress(true);
 		serverSocketChannel.socket().bind(new InetSocketAddress(port));
 		System.out.println("服务器启动....");
 	}
-	
-	public void service(){
-		while(true){
-			SocketChannel socketChannel=null;
-			try{
-				socketChannel=serverSocketChannel.accept();
+
+	public void service() {
+		while (true) {
+			SocketChannel socketChannel = null;
+			try {
+				socketChannel = serverSocketChannel.accept();
 				executorService.execute(new Handler(socketChannel));
-			}catch(IOException e){
+			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
 	}
-	
-	public static void main(String[] args) throws IOException{
+
+	public static void main(String[] args) throws IOException {
 		new SimpleHttpServer().service();
 	}
-	
-	class Handler implements Runnable{ //Handler是内部类，负责处理HTTP请求
+
+	class Handler implements Runnable { // Handler是内部类，负责处理HTTP请求
 		private SocketChannel socketChannel;
-       public Handler(SocketChannel socketChannel){
-			this.socketChannel=socketChannel;
+
+		public Handler(SocketChannel socketChannel) {
+			this.socketChannel = socketChannel;
 		}
 
 		@Override
 		public void run() {
-			// TODO Auto-generated method stub
 			handle(socketChannel);
 		}
-		
-		
-		
-		
-		public void handle(SocketChannel socketChannel){
-			try{
-				Socket socket=socketChannel.socket();
-				System.out.println("接收到客户连接，来自："+socket.getInetAddress()+":"+socket.getPort());
-				
-				ByteBuffer buffer=ByteBuffer.allocate(1024);
-				socketChannel.read(buffer);//接收HTTP请求，假定其长度不超过1024个字节
+
+		public void handle(SocketChannel socketChannel) {
+			try {
+				Socket socket = socketChannel.socket();
+				System.out.println("接收到客户连接，来自：" + socket.getInetAddress() + ":" + socket.getPort());
+
+				ByteBuffer buffer = ByteBuffer.allocate(1024);
+				socketChannel.read(buffer);// 接收HTTP请求，假定其长度不超过1024个字节
 				buffer.flip();
-				String request=decode(buffer);
+				String request = decode(buffer);
 				System.out.println(request);
-				
-				
-				//生成HTTP响应结果
-				StringBuffer sb=new StringBuffer("HTTP/1.1 200 OK\r\n");
+
+				// 生成HTTP响应结果
+				StringBuffer sb = new StringBuffer("HTTP/1.1 200 OK\r\n");
 				sb.append("Content-Type:text/html\r\n\r\n");
-				socketChannel.write(encode(sb.toString()));//发送HTTP响应的第一行和响应头
-				
+				socketChannel.write(encode(sb.toString()));// 发送HTTP响应的第一行和响应头
+
 				FileInputStream in;
-				//获取HTTP请求的第一行
-				String firstLineOfRequest=request.substring(0, request.indexOf("\r\n"));
-				if(firstLineOfRequest.indexOf("login.html")!=-1){
-					in=new FileInputStream("log.html");
+				// 获取HTTP请求的第一行
+				
+				int endLength=request.indexOf("\r\n");
+				
+				if(endLength!=-1 ){
+					String firstLineOfRequest = request.substring(0, endLength);
+					if (firstLineOfRequest.indexOf("login.html") != -1) {
+						in = new FileInputStream("log.html");
+					} else {
+						in = new FileInputStream("objectFile.obj");
+					}
 				}else{
-					in=new FileInputStream("hello.html");
+					in = new FileInputStream("objectFile.obj");
 				}
-				FileChannel fileChannel=in.getChannel();
-				fileChannel.transferTo(0, fileChannel.size(), socketChannel);//发送响应正文
 				
-				
-				
-			}catch(Exception e){
+				FileChannel fileChannel = in.getChannel();
+				fileChannel.transferTo(0, fileChannel.size(), socketChannel);// 发送响应正文
+
+			} catch (Exception e) {
 				e.printStackTrace();
-			}finally{
-				try{
-					if(socketChannel!=null)socketChannel.close();
-				}catch(Exception e){
+			} finally {
+				try {
+					if (socketChannel != null)
+						socketChannel.close();
+				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
 		}
-		
-		private Charset charset=Charset.forName("GBK");
-		public String decode(ByteBuffer buffer){ //解码
-		 CharBuffer charBuffer=charset.decode(buffer);
-		 return charBuffer.toString();
+
+		private Charset charset = Charset.forName("GBK");
+
+		public String decode(ByteBuffer buffer) { // 解码
+			CharBuffer charBuffer = charset.decode(buffer);
+			return charBuffer.toString();
 		}
-		public ByteBuffer encode(String str){//编码
+
+		public ByteBuffer encode(String str) {// 编码
 			return charset.encode(str);
 		}
-		
-		
-		
+
 	}
 
 }
