62a7fe029155522990073b35b36d1ac256aebba0
barry <1096490965@qq.com>
2016-02-25 15:17:05 +0800

    ch08

--- netTest/src/com/ch08/EchoClient.java
+++ netTest/src/com/ch08/EchoClient.java
@@ -1,5 +1,55 @@
 package com.ch08;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
 public class EchoClient {
+	
+	private String remoteHost="localhost";
+	private int remotePort=8000;
+	private DatagramSocket socket;
+	
+	public EchoClient() throws IOException{
+		socket=new DatagramSocket();//与本地的任意一个UDP端口绑定	
+	}
+	
+	public static void main(String[] args) throws IOException{
+		new EchoClient().talk();
+	}
+	
+	public void talk() throws IOException{
+		
+		try{
+			
+			InetAddress remoteIP=InetAddress.getByName(remoteHost);
+			BufferedReader localReader=new BufferedReader(new InputStreamReader(System.in));
+			
+			String msg=null;
+			while((msg=localReader.readLine())!=null){
+				byte[] outputData=msg.getBytes();
+				DatagramPacket outputPacket=new DatagramPacket(outputData,outputData.length,remoteIP,remotePort);
+				
+				socket.send(outputPacket);//给EchoServer发送数据报
+				DatagramPacket inputPacket=new DatagramPacket(new byte[512],512);
+				socket.receive(inputPacket);//接收EchoServer的数据报
+				System.out.println(new String(inputPacket.getData(),0,inputPacket.getLength()));
+				
+				if(msg.equals("bye")){
+					break;
+				}
+			}
+			
+		}catch(Exception e){
+			e.printStackTrace();
+		}finally{
+			socket.close();
+		}
+		
+	}
+	
 
 }
--- netTest/src/com/ch08/EchoServer.java
+++ netTest/src/com/ch08/EchoServer.java
@@ -1,6 +1,46 @@
 package com.ch08;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
 public class EchoServer {
+	
+	private int port=8000;
+	
+	private DatagramSocket socket;
+	
+	public EchoServer() throws IOException{
+		socket=new DatagramSocket(port);//与本地的一个固定端口绑定
+		System.out.println("服务器启动");
+	}
+	
+	public String echo(String msg){
+		return "echo:"+msg;
+	}
+	
+	
+	public void service(){
+		
+		while(true){
+			try{
+				DatagramPacket packet=new DatagramPacket(new byte[512],512);
+				socket.receive(packet);//接收来自任意一个EchoClient的数据源
+				String msg=new String(packet.getData(),0,packet.getLength());
+				System.out.println(packet.getAddress()+":"+packet.getPort()+">"+msg);
+				
+				packet.setData(echo(msg).getBytes());
+				socket.send(packet);//给EchoClient回复一个数据报
+			}catch(IOException e){
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public static void main(String[] args)throws IOException{
+		new EchoServer().service();
+	}
+	
 	
 	
 	
