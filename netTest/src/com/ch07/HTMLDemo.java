10682090a5a2c723cc1860ad8a167f4416a954ea
barry <1096490965@qq.com>
2016-02-25 14:41:00 +0800

    ch07

--- netTest/src/com/ch07/HTMLDemo.java
+++ netTest/src/com/ch07/HTMLDemo.java
@@ -1,6 +1,8 @@
 package com.ch07;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class HTMLDemo extends JFrame{
 
@@ -8,6 +10,19 @@ public class HTMLDemo extends JFrame{
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	
+	private JLabel jLabel;
+	private JButton jButton;
+	
+	public HTMLDemo(String title){
+		super(title);
+		jLabel=new JLabel("<html></html>");
+		//设定go.jpg文件与HTMLDemo.class文件位置同一个目录下
+		jButton=new JButton("");
+		//设置鼠标移动到该Button时的提示信息
+		
+		
+	}
 	
 	
 	
