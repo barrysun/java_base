393fd773116f1fced5c10a4001bb6b4fa4831071
own <Administrator@WRGHO-60304dfv>
2016-05-04 14:19:09 +0800

    NIO threadPool

--- ioTest/src/com/nio/SelectSocketsThreadPool.java
+++ ioTest/src/com/nio/SelectSocketsThreadPool.java
@@ -36,11 +36,31 @@ public class SelectSocketsThreadPool extends SelectSockets {
 			//Fill up the pool with worker threads
 			for(int i=0;i<poolSize;i++){
 				WorkerThread thread=new WorkerThread(this);
-				//Set thread name for deugging
+				//Set thread name for deugging. Start it
+				thread.setName("Worker"+(i+1));
+				thread.start();
+				idle.add(thread);
 			}
+		}
+		/**
+		 * 
+		 */
+		WorkerThread getWorker(){
+			WorkerThread worker=null;
+			synchronized(idle){
+				if(idle.size()>0){
+					worker=(WorkerThread)idle.remove(0);
+				}
+			}
+			return (worker);
 		}
 	}
 	
+	/**
+	 * A worker thread class which can drain
+	 * @author Administrator
+	 *
+	 */
 	private class WorkerThread extends Thread{
 		private ByteBuffer buffer=ByteBuffer.allocate(1024);
 		private ThreadPool pool;
